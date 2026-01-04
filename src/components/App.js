47d37d7683719dd817a7aced90833160582101ca
Tejas Suryawanshi <143705472+t6jas@users.noreply.github.com>
2026-01-04 19:15:10 +0530

    Implemented button counter using useState

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,19 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [count, setCount] = useState(0);
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <p>Button clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click Me
+      </button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
